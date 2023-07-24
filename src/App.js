e0e86cf7e63556ee4a513eed7f230e070b7bd526
edymol <edymolinap@gmail.com>
2023-07-24 16:24:12 -0700

    fix: project restructure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Header from './components/Header';
+import DatingCards from './components/DatingCards';
+import SwipeButtons from './components/SwipeButtons';
+
+
 function App() {
-    return (
-        <div className="app">
-            <Header />
-        </div>
-    );
+return (
+<div className="app">
+<Header />
+< DatingCards />
+<SwipeButtons />
+</div>
+);
 }
 export default App;
